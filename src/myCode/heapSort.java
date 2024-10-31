29a593a8a3eeb00269891ec603f45d3a9a7df246
Taylorking01 <taylorkinginbox@gmail.com>
2024-10-31 14:00:07 +0000

    Heapsort function

--- src/myCode/heapSort.java
+++ src/myCode/heapSort.java
@@ -1,4 +1,30 @@
 package myCode;
 
 public class heapSort {
+
+    public static void heapsort(int[] arr) {
+        int n = arr.length;
+
+        //Build max heap#
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            //heapify(arr, n, i);
+        }
+
+        for (int i = n - 1; i >= 1; i--) {
+            //swap(arr, 0, i);
+            //heapify(arr, i, 0);
+        }
+    };
+
+    public static void main(String[] args) {
+        int[] arr = {8,6,5,9,3,1,2};
+
+        heapsort(arr);
+
+        System.out.println("Sorted array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+
+    }
 }
