ddd8ac68e83b800aed4829e3547af28d06abaf50
Taylorking01 <taylorkinginbox@gmail.com>
2024-10-31 14:10:01 +0000

    Heapsort functional

--- src/myCode/heapSort.java
+++ src/myCode/heapSort.java
@@ -3,6 +3,27 @@ package myCode;
 public class heapSort {
 
 
+    public static void heapify(int[] arr, int n, int i) {
+        int largest = i;
+        int left = 2 * i +1;
+        int right = 2 * i + 2;
+
+        //Check if left child exists and is greater than root
+        if (left < n && arr[left] > arr[largest]) {
+            largest = left;
+        }
+
+        // Check if the right child exists and is greater than the current largest
+        if (right < n && arr[right] > arr[largest]) {
+            largest = right;
+        }
+
+        // If largest is not the root
+        if (largest != i) {
+            swap(arr, i, largest);
+            heapify(arr, n, largest);
+        }
+    }
     public static void swap(int[] arr, int i, int j) {
         int temp = arr[i];
         arr[i] = arr[j];
@@ -13,12 +34,12 @@ public class heapSort {
 
         //Build max heap#
         for (int i = n / 2 - 1; i >= 0; i--) {
-            //heapify(arr, n, i);
+            heapify(arr, n, i);
         }
 
         for (int i = n - 1; i >= 1; i--) {
-            //swap(arr, 0, i);
-            //heapify(arr, i, 0);
+            swap(arr, 0, i);
+            heapify(arr, i, 0);
         }
     };
 
